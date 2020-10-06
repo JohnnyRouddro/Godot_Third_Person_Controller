ef461621206dfe888834fc17aa1594d0dffe2e5b
JohnnyRouddro <johnnyrouddro@yahoo.com>
2020-10-06 10:00:05 +0600

    Add files via upload

--- Player/Camera.gd
+++ Player/Camera.gd
@@ -15,6 +15,7 @@ func _ready():
 	
 func _input(event):
 	if event is InputEventMouseMotion:
+		$mouse_control_stay_delay.start()
 		camrot_h += -event.relative.x * h_sensitivity
 		camrot_v += event.relative.y * v_sensitivity
 		
@@ -22,5 +23,17 @@ func _physics_process(delta):
 	
 	camrot_v = clamp(camrot_v, cam_v_min, cam_v_max)
 	
-	$h.rotation_degrees.y = lerp($h.rotation_degrees.y, camrot_h, delta * h_acceleration)
+	var mesh_front = get_node("../Mesh").global_transform.basis.z
+	var rot_speed_multiplier = 0.15 #reduce this to make the rotation radius larger
+	var auto_rotate_speed =  (PI - mesh_front.angle_to($h.global_transform.basis.z)) * get_parent().velocity.length() * rot_speed_multiplier
+	
+	if $mouse_control_stay_delay.is_stopped():
+		#FOLLOW CAMERA
+		$h.rotation.y = lerp_angle($h.rotation.y, get_node("../Mesh").global_transform.basis.get_euler().y, delta * auto_rotate_speed)
+		camrot_h = $h.rotation_degrees.y
+	else:
+		#MOUSE CAMERA
+		$h.rotation_degrees.y = lerp($h.rotation_degrees.y, camrot_h, delta * h_acceleration)
+	
 	$h/v.rotation_degrees.x = lerp($h/v.rotation_degrees.x, camrot_v, delta * v_acceleration)
+	
